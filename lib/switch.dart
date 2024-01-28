b59b5b30a58d66d7990ff64b4eb768445791c2a0
Boodytaha <bouditaha80@gmail.com>
2024-01-28 20:40:26 +0200

    addedfolder

---
+++ lib/switch.dart
@@ -0,0 +1,19 @@
+void main (){
+  int num = 33;
+
+  switch (num){
+    case 10:
+    case 33:
+      print ("messi");
+    case 7:
+      print ("cristiano");
+    case 9: 
+      print ("mbabe");
+    case 1:
+      print ("Alisson");  
+    case 5:
+      print ("sergio ramos");
+    default:
+      print("no such player with this number");        
+  }
+}
